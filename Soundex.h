c436ad471687654d5fc21864089237299e776edb
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-05 10:29:48 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,32 +6,21 @@
 #include <string.h>
 
 char getSoundexCode(char c) {
-    static char soundexTable[256];
-    static int initialized = 0;
-    if (!initialized) {
-        for (int i = 0; i < 256; ++i) {
-            soundexTable[i] = '0'; 
-        }
-        for (char c : "AEIOUHWY") {
-            soundexTable[(unsigned char)c] = '0';
-        }
-        for (char c : "BFVP") {
-            soundexTable[(unsigned char)c] = '1';
-        }
-        for (char c : "CGJKQSXZ") {
-            soundexTable[(unsigned char)c] = '2';
-        }
-        for (char c : "DT") {
-            soundexTable[(unsigned char)c] = '3';
-        }
-        soundexTable[(unsigned char)'L'] = '4';
-        for (char c : "MN") {
-            soundexTable[(unsigned char)c] = '5';
-        }
-        soundexTable[(unsigned char)'R'] = '6';
-        initialized = 1;
-    }
-    return soundexTable[(unsigned char)toupper(c)];
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'H' || c == 'W' || c == 'Y') 
+        return '0';
+    if (c == 'B' || c == 'F' || c == 'P' || c == 'V')
+        return '1';
+    if (c == 'C' || c == 'G' || c == 'J' || c == 'K' || c == 'Q' || c == 'S' || c == 'X' || c == 'Z') 
+        return '2';
+    if (c == 'D' || c == 'T') 
+        return '3';
+    if (c == 'L') 
+        return '4';
+    if (c == 'M' || c == 'N')
+        return '5';
+    if (c == 'R')
+        return '6';
+    return '0';  
 }
 
 void generateSoundexPattern(int len,int sIndex, const char *name, char *soundex) {
