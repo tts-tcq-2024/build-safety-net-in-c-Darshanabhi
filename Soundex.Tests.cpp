e09b4c416ceeaf621e949b9c19a35adbe3626480
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-05 11:35:49 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,15 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
+  generateSoundex("HARI", soundex);
+  generateSoundex("VINU", soundex);
+  generateSoundex("JOY", soundex);
+  generateSoundex("HI", soundex);
+  generateSoundex("HARSH", soundex);
+  generateSoundex("MAN", soundex);
+  generateSoundex("WU", soundex);
+  generateSoundex("BC", soundex);
+  generateSoundex("DASH", soundex);
+ 
  // ASSERT_EQ(soundex,"A200");
 }
