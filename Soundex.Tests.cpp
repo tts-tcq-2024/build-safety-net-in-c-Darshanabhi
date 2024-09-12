8e7673a74687e4cdda42238e199a3b6ca7f62a69
Darshanabhi <88896158+Darshanabhi@users.noreply.github.com>
2024-09-12 20:41:11 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,15 +5,16 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  generateSoundex("HARI", soundex);
-  generateSoundex("VINU", soundex);
-  generateSoundex("JOY", soundex);
-  generateSoundex("HI", soundex);
-  generateSoundex("HARSH", soundex);
-  generateSoundex("MAN", soundex);
-  generateSoundex("WU", soundex);
-  generateSoundex("BC", soundex);
-  generateSoundex("DASH", soundex);
+  generateSoundex("ROBERT", soundex);
+  generateSoundex("JACKSON", soundex);
+  generateSoundex("SASSOON", soundex);
+  generateSoundex("SMITH123", soundex);
+  generateSoundex(" ", soundex);
+  generateSoundex("MAN HU", soundex);
+  generateSoundex("65342", soundex);
+  generateSoundex("356GHFK", soundex);
+  generateSoundex("HJ@LMN", soundex);
+  generateSoundex("M*BH8NDK", soundex);
  
  // ASSERT_EQ(soundex,"A200");
 }
